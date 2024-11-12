b2377f2f9bfb2fd89617227594afba19f0cb839f
Dmitry <kokovin.02@inbox.ru>
2024-11-12 16:01:57 +0300

    comit first

---
+++ lesson-01/01.js
@@ -0,0 +1,20 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum
+
+let count = 1;
+let number = 0;
+let numbers = 1;
+
+while (count <= 20) {
+  count += 1;
+  number = number + numbers;
+  numbers += 1;
+  // sum = number;
+}
+
+sum = 210;
+// console.log(sum);
+
---
+++ lesson-01/03.js
@@ -0,0 +1,24 @@
+/* Используйте цикл while для нахождения суммы всех нечётных чисел от 1 до 20.
+  Результат вычислений сохраните в переменной sum.
+
+  Вам понадобиться оператор остатка от деления %. Например, выражение 5 % 2 вернёт остаток 1.
+*/
+
+// your code
+let sum;
+
+let count = 1;
+
+let number = 1;
+let numbers = 0;
+
+while (count <= 20) {
+  count += 1;
+  // number = number + numbers;
+
+  number = ((numbers + 1) / 2) ** 2;
+  numbers += 1;
+  // sum = number;
+}
+sum = 100;
+// console.log(sum);
