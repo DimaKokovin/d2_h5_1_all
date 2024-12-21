22859d8fc2a8086d592ebeee9eb5591bc48330d8
Dmitry <kokovin.02@inbox.ru>
2024-12-21 18:06:54 +0300

    first commit25

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -31,45 +31,27 @@ console.log(gallery)
 }
 */
 
-// const gallery = {
-//   "Mona Lisa": "Leonardo da Vinci",
-//   "Starry Night": "Vincent van Gogh",
-//   "The Scream": "Edvard Munch",
-// };
+const gallery = {
+  "Mona Lisa": "Leonardo da Vinci",
+  "Starry Night": "Vincent van Gogh",
+  "The Scream": "Edvard Munch",
+};
 
-// function updateGallery(obj, name_of_work, new_value) {
-//   if (name_of_work in obj) {
-//     for (key in obj) {
-//       if (name_of_work === key) {
-//         obj[key] = new_value;
-//       }
-//     }
-//   } else {
-//     obj[name_of_work] = new_value;
-//   }
-// }
+function updateGallery(obj, name_of_work, new_value) {
+  if (name_of_work in obj) {
+    for (key in obj) {
+      if (name_of_work === key) {
+        obj[key] = new_value;
+      }
+    }
+  } else {
+    obj[name_of_work] = new_value;
+  }
+}
+// console.log( updateGallery() {}
+updateGallery(gallery, "Mona Lisa", "Leonardo da Vinci, 1503-1506");
+updateGallery(gallery, "The Persistence of Memory", "Salvador Dali");
 
-// function updateGallery() {}
-// updateGallery(gallery, "Mona Lisa", "Leonardo da Vinci, 1503-1506");
-// updateGallery(gallery, "The Persistence of Memory", "Salvador Dali");
+console.log(gallery);
 
-// console.log(gallery);
-
-// sum(2, 3);
-// function sum(a, b) {
-//   a + b;
-// }
-
-// console.log(arrowSum(5, 5));
-// let arrowSum = (a, b) => {
-//   return a + b;
-// };
-
-// console.log(sum(2, 3));
-
-// console.log(arrowSum(5, 5));
-
-// console.log("now");
-// setTimeout(() => {
-//   console.log("time");
-// }, 3000);
+// console.log(updateGallery())
