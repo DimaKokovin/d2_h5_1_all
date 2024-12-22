88733e9cc071c5396f7ba738c48ac55551c36ab4
Dmitry <kokovin.02@inbox.ru>
2024-12-22 19:58:55 +0300

    first commit29

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,10 +4,27 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
+// function isNumeric(str) {
+//   if (Number.isInteger(str)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+//   // your code
+// }
+
 function isNumeric(str) {
+  if (Number(str)) {
+    return true;
+  } else {
+    return false;
+  }
+
   // your code
 }
 
+console.log(isNumeric("12.3"));
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
