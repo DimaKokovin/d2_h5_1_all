91a7719a454023dbe3c519e5bfdf74e77b010199
Dmitry <kokovin.02@inbox.ru>
2025-01-09 17:02:14 +0300

    first commit55

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -10,7 +10,8 @@
 - `element` (текущий элемент массива)
 - `index` (индекс текущего элемента)
 
-Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, для которых `callback` возвращает `true`.
+Функция `filter` должна возвращать новый массив, содержащий только те элементы `array`, 
+для которых `callback` возвращает `true`.
 */
 
 /* Пример использования:
@@ -23,4 +24,27 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+  let ar = [];
+  for (let index = 0; index < array.length; index++) {
+    if (callback(array[index], index)) {
+      ar.push(array[index]);
+    }
+  }
+  return ar;
+};
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const oddNumbers = filter(numbers, (element, index) => {
+//   return element % 2 !== 0;
+// });
+
+// console.log(oddNumbers);
+
+// const a = [1, 2, 3, 4, 5, 6, 7, 8];
+// const newarrr = a.filter((el, i) => {
+//   return el > 3;
+// });
+
+// console.log(newarrr);
--- lesson-09/02.js
+++ lesson-09/02.js
@@ -9,7 +9,8 @@
 - `element` (текущий элемент массива),
 - `index` (индекс текущего элемента),
 
-Ваша функция `map` должна возвращать новый массив, где каждый элемент является результатом вызова функции `callback` на каждом элементе исходного массива.
+Ваша функция `map` должна возвращать новый массив, где каждый элемент
+ является результатом вызова функции `callback` на каждом элементе исходного массива.
 */
 
 /*
@@ -24,4 +25,49 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  let a = [];
+  for (let index = 0; index < array.length; index++) {
+    let g = callback(array[index], index);
+    a.push(g);
+  }
+  return a;
+};
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const doubledNumbers = map(numbers, (element, index) => {
+//   return element * 2;
+// });
+
+// console.log(doubledNumbers); // Должен вывести: [2, 4, 6, 8, 10]
+
+// const elX3 = (el, i) => {
+//   return el * 3;
+// };
+
+// console.log(map(numbers, elX3));
+
+// const k = (el, i) => {
+//   return el + "a";
+// };
+
+// console.log(map(numbers, k));
+
+// let gg = ["a", "bb", "c"];
+
+// const l = (el, i) => {
+//   return el.toUpperCase();
+// };
+
+// console.log(map(gg, l));
+
+// const f = (el, i) => {
+//   if (i > 1) {
+//     return el.toUpperCase();
+//   } else {
+//     return el;
+//   }
+// };
+
+// console.log(map(gg, f));
